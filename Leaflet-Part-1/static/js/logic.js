dbd4e8b499bf1fe76d8afe92ea9bc07a773b69c5
FESSEHA HABTIYMER <63689919+fishde2020@users.noreply.github.com>
2023-12-07 23:12:05 -0500

    Update logic.js

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,116 +1,116 @@
-// Create tile layers of map
-let greyscale = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+// store geoJSON
+const link = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-// Set up URL
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson';
-
-// Set up geoJSON request
-d3.json(url).then(function (data) {
-    console.log(data);
-    
+// perform a GET request to the query URL
+d3.json(link).then((data) => {
+    // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
+    console.log(data.features);
 });
-
-// Function to determine marker size
-function markerSize(magnitude) {
-    return magnitude * 100000;
-};
-
-// Function to determine marker color by depth
-function chooseColor(depth) {
-    if (depth < 10) return "#64B5F6";
-    else if (depth < 30) return "#43A047";
-    else if (depth < 50) return "#FFF176";
-    else if (depth < 70) return "#FB8C00";
-    else if (depth < 90) return "#B71C1C";
-    else return "#FF3300";
-}
-
-function createFeatures(earthquakeData) {
-
-  // Give each feature a popup that describes the place and time of the earthquake.
-    function onEachFeature(feature, layer) {
-        layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date: ${new Date(feature.properties.time)}
-        </p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
-    }
-    // Run the onEachFeature function once for each piece of data in the array.
-    var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature,
-
-        // Point to layer used to alter markers
-        pointToLayer: function (feature, latlng) {
-
-            // Determine the style of markers based on properties
-            var markers = {
-                radius: markerSize(feature.properties.mag),
-                fillColor: chooseColor(feature.geometry.coordinates[2]),
-                fillOpacity: 0.5,
-                color: "black",
-                stroke: true,
-                weight: 1
-            }
-            return L.circle(latlng, markers);
-        }
-    });
-
-    // Send our earthquakes layer to the createMap function/
-    createMap(earthquakes);
-}
 
 function createMap(earthquakes) {
-
-    // Create the base layers.
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-
-    var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    // assign the different mapbox styles
+    const satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 20,
+        id: 'mapbox.satellite',
+        accessToken: API_KEY
     });
 
-    // Create a baseMaps object.
-    var baseMaps = {
-        "Street Map": street,
-        "Topographic Map": topo
+    const grayscale = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 20,
+        id: 'mapbox.light',
+        accessToken: API_KEY
+    });
+
+    const outdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 20,
+        id: 'mapbox.outdoors',
+        accessToken: API_KEY
+    });
+
+    const baseMap = {
+        'Satellite': satellite,
+        'Grayscale': grayscale,
+        'Outdoors': outdoors
     };
 
-    // Create an overlay object to hold our overlay.
-    var overlayMaps = {
+    const overlayMap = {
         Earthquakes: earthquakes
     };
 
-    // Create our map, giving it the streetmap and earthquakes layers to display on load.
-    var myMap = L.map("map", {
-        center: [41, 35],
-        zoom: 3,
-        layers: [street, earthquakes]
+    const myMap = L.map('map', {
+        center: [36.7126875, -120.476189],
+        zoom: 4,
+        layers: [outdoors, earthquakes]
     });
 
-    // Create a layer control.
-    // Pass it our baseMaps and overlayMaps.
-    // Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps, {
+    L.control.layers(baseMap, overlayMap, {
         collapsed: false
     }).addTo(myMap);
 
-    // Create legend 
-    var legend = L.control({ position: "bottomright" });
+    // function to assign colors for legend and markers
+    function getColor(d) {
+        return d > 5 ? '#f06b6b' :
+            d > 4 ? '#f0936b' :
+            d > 3 ? '#f3ba4e' :
+            d > 2 ? '#f3db4c' :
+            d > 1 ? '#e1f34c' :
+                    '#b7f34d';
+    }
 
-    legend.onAdd = function (myMap) {
-        var div = L.DomUtil.create("div", "legend");
-        div.innerHTML += "<h4 style='text-align: center'>Legend by Depth (km)</h4>";
-        div.innerHTML += '<i style="background: #64B5F6"></i><span>10 km or less</span><br>';
-        div.innerHTML += '<i style="background: #43A047"></i><span>30 km or less</span><br>';
-        div.innerHTML += '<i style="background: #FFF176"></i><span>50 km or less</span><br>';
-        div.innerHTML += '<i style="background: #FB8C00"></i><span>70 km or less</span><br>';
-        div.innerHTML += '<i style="background: #FF3300"></i><span>90 km or less</span><br>';
-        div.innerHTML += '<i style="background: #B71C1C"></i><span>More than 90 km</span><br>';
-            return div;
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function(myMap) {
+        const div = L.DomUtil.create('div', 'info legend')
+        const magnitudes = [0, 1, 2, 3, 4, 5]
+        const labels = []
+
+        for (let i = 0; i < magnitudes.length; i++) {
+            div.innerHTML +=
+            '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i>' + magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+        }
+        return div
     };
-
     legend.addTo(myMap);
+
 }
-   
+
+function createFeatures(eqdata) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup('<h4>Place: ' + feature.properties.place + '</h4><h4>Date: ' + new Date(feature.properties.time) + '</h4><h4>Magnitude: ' + feature.properties.mag + '</h4><h4>USGS Event Page: <a href=' + feature.properties.url + " target='_blank'>Click here</a></h4>", {maxWidth: 400})
+    }
+
+    const layerToMap = L.geoJSON(eqdata, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function(feature, latlng) {
+            let radius = feature.properties.mag * 4.5;
+
+            if (feature.properties.mag > 5) {
+                fillcolor = '#f06b6b';
+            }
+            else if (feature.properties.mag >= 4) {
+                fillcolor = '#f0936b';
+            }
+            else if (feature.properties.mag >= 3) {
+                fillcolor = '#f3ba4e';
+            }
+            else if (feature.properties.mag >= 2) {
+                fillcolor = '#f3db4c';
+            }
+            else if (feature.properties.mag >= 1) {
+                fillcolor = '#e1f34c';
+            }
+            else  fillcolor = '#b7f34d';
+
+            return L.circleMarker(latlng, {
+                radius: radius,
+                color: 'black',
+                fillColor: fillcolor,
+                fillOpacity: 1,
+                weight: 1
+            });
+        }
+    });
+    createMap(layerToMap);
+}
   
