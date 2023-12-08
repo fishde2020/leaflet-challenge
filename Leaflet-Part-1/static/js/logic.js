a920890a2e11af3edeb9c95d97fa4f8472de84b4
FESSEHA HABTIYMER <63689919+fishde2020@users.noreply.github.com>
2023-12-07 23:26:09 -0500

    Update logic.js

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,97 +1,124 @@
-// Create an initial map object
-// Set the longitude, latitude, and the starting zoom level
-let myMap = L.map("map",{
-    center:[37.09, -95.71], 
-    zoom: 5});
+// Creating the map object
+let myMap = L.map("map", {
+    center: [27.96044, -82.30695],
+    zoom: 3
+  });
+  
+  // Adding the tile layer
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+  
+  // Load the GeoJSON data.
+let geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+const TECTONIC_URL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
-// Add a tile layer (the background map image) to our map
-// Use the addTo method to add objects to our map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+  let earthquakeGroup = new L.LayerGroup();
+  let tectonicGroup = new L.LayerGroup();
+
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  let overlays = {
+    Earthquakes: earthquakeGroup,
+    Tectonic: tectonicGroup
+  };
+  
+  // Get the data with d3.
+  d3.json(geoData).then(function(data) {
+  
+ 
+  function getValue(x) {
+    return x > 90 ? "#F06A6A" :
+          x > 70 ? "#F0A76A" :
+          x > 50 ? "#F3B94C" :
+          x > 30 ? "#F3DB4C" :
+          x > 10 ? "#E1F34C" :
+        
+            "#B6F34C";
+  }
+
+ 
+  function style(feature) {
+    return {
+      "stroke": true,
+          radius: feature.properties.mag * 3,
+      fillColor: getValue(feature.geometry.coordinates[2]),
+      color: "black",
+      weight: 0.5,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
+}
 
 
-// Store our API endpoint as queryUrl.
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-d3.json(queryUrl).then(function (data) {
-    // Console log URL response to check the data.
-    console.log(data.features)
-    console.log(data.features.length)
-
-    for (let i = 0; i < data.features.length; i++) {
-
-        let location = data.features[i].geometry;
-
-        let color= "";
-        if(location.coordinates[2] >90) {
-            color = "#c45e5e";
-        }
-        else if (location.coordinates[2] >=70) {
-            color = "#c4795e";
-        }
-        else if (location.coordinates[2] >=50) {
-            color = "#c4b05e";
-        }
-        else if (location.coordinates[2] >=30) {
-            color = "#bdc45e";
-        }
-        else if (location.coordinates[2] >=10) {
-            color = "#a2c45e";
-        }
-        else if (location.coordinates[2] >=-10) {
-            color = "#87c45e";
-        }
-        else {
-            color = "";
-        }
-
-
-        L.circle([location.coordinates[1], location.coordinates[0]],{
-            fillOpacity:1,
-            color:"black",
-            weight:1,
-            fillColor:color,
-            radius: data.features[i].properties.mag * 20000
-        })
-        .bindPopup(`<h3>${data.features[i].properties.place}</h3>
-        <p><b>Longitude: </b>${location.coordinates[0]}; <b>Latitude: </b>${location.coordinates[1]}<br>
-        <b>Magnitude: </b>${data.features[i].properties.mag}<br>
-        <b>Depth: </b>${location.coordinates[2]}km
-        </p>
-        <hr>
-        <p>${new Date(data.features[i].properties.time)}</p>`)
-        .addTo(myMap);
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, style(feature));
+    },
     
+        // Binding a popup to each layer
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup("<strong>" + feature.properties.place + "</strong><br /><br />Magnitude: " +
+            feature.properties.mag + "<br /><br />depth: " + feature.geometry.coordinates[2]);
+        }
+      }).addTo(earthquakeGroup);
+
+      earthquakeGroup.addTo(myMap);
+
+      
+      let legend = L.control({ position: "bottomright" });
+
+      legend.onAdd = function() {
+
+          let div = L.DomUtil.create("div", "info legend");
+          //labels = ["<10", "10-30", "30-50", "50-70", "70-90", "90+"];
+          let grades = [-10, 10, 30, 50, 70, 90];
+          let colors = ["#98ee00","#d4ee00","#eecc00","#ee9c00","#ea822c","#ea2c2c"];
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (let i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + colors[i] + '">&emsp;&emsp;</i> '
+              + grades[i]
+              + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
       }
 
-      //Add a legend
-      //Create a control with position
-      var legend = L.control({position: 'bottomright'});
+      return div;
+      };
 
-      legend.onAdd = function () {
-      
-          let div = L.DomUtil.create('div', 'info legend');
-          let dep = [-10, 10, 30, 50, 70, 90];
-
-          function getColor(d) {
-            return d > 90 ? "#c45e5e":
-                   d >= 70  ? "#c4795e":
-                   d >= 50  ? "#c4b05e":
-                   d >= 30  ? "#bdc45e":
-                   d >= 10   ? "#a2c45e":
-                   d >= -10   ? "#87c45e":
-                              '';
-        }
-      
-          // loop through our density intervals and generate a label with a colored square for each interval
-          for (var i = 0; i < dep.length; i++) {
-              div.innerHTML +=
-                  '<i style="background:' + getColor(dep[i]) + '"> </i> ' +
-                  dep[i] + (dep[i + 1] ? '&ndash;' + dep[i + 1] + '<br>' : '+');
-                }
-                return div;
-            };
-    
       legend.addTo(myMap);
+
+
 });
+  
+// Get the data with d3.
+d3.json(TECTONIC_URL).then(function(plate_data) {  
+ 
+  let tectonicPlates = L.geoJson(plate_data, {
+    style: {
+      color: "blue",
+      weight: 2,
+      opacity: 1
+    }
+  });
+    
+  tectonicPlates.addTo(tectonicGroup);
+  tectonicGroup.addTo(myMap);
+});
+
+
+var layersControl = L.control.layers(null, null, { collapsed: false });
+
+// Add overlay layers to layer control
+layersControl.addOverlay(earthquakeGroup, "Earthquakes");
+layersControl.addOverlay(tectonicGroup, "Tectonic Plates");
+layersControl.addBaseLayer(street, "Map");
+layersControl.addBaseLayer(topo, "Topo Map");
+
+// Add the layer control to the map
+layersControl.addTo(myMap);
