afa670c93cfb04f00e1dc0c19d1387c1908915ec
FESSEHA HABTIYMER <63689919+fishde2020@users.noreply.github.com>
2023-12-12 13:02:06 -0500

    Update logic.js

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,124 +1,80 @@
-// Creating the map object
-let myMap = L.map("map", {
-    center: [27.96044, -82.30695],
-    zoom: 2
-  });
-  
-  // Adding the tile layer
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-  
-  // Load the GeoJSON data.
-let geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-const TECTONIC_URL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+// Initialize the map
+var myMap = L.map("map", {
+  center: [37.09, -95.71], // Adjust the center as needed
+  zoom: 5
+});
 
-  let earthquakeGroup = new L.LayerGroup();
-  let tectonicGroup = new L.LayerGroup();
+// Add Mapbox tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaHhwcmluY2l2aWwiLCJhIjoiY2xwcGxkNW1xMDF0eDJpbXo3YTBnbWp0cyJ9.eQkMdFert-in9BCcnzK_xw', {
+  maxZoom: 18,
+  id: 'mapbox/streets-v11', // Choose the style that fits your map
+  tileSize: 512,
+  zoomOffset: -1
+}).addTo(myMap);
 
-  let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
-  };
+// URL for the USGS Earthquake GeoJSON data
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-  let overlays = {
-    Earthquakes: earthquakeGroup,
-    Tectonic: tectonicGroup
-  };
-  
-  // Get the data with d3.
-  d3.json(geoData).then(function(data) {
-  
- 
-  function getValue(x) {
-    return x > 90 ? "#F06A6A" :
-          x > 70 ? "#F0A76A" :
-          x > 50 ? "#F3B94C" :
-          x > 30 ? "#F3DB4C" :
-          x > 10 ? "#E1F34C" :
-        
-            "#B6F34C";
-  }
-
- 
-  function style(feature) {
-    return {
-      "stroke": true,
-          radius: feature.properties.mag * 3,
-      fillColor: getValue(feature.geometry.coordinates[2]),
-      color: "black",
-      weight: 0.5,
-      opacity: 1,
-      fillOpacity: 0.8
-    };
+// Function to determine marker size based on earthquake magnitude
+function markerSize(magnitude) {
+  return magnitude * 3;
 }
 
+// Function to determine marker color based on earthquake depth
+function markerColor(depth) {
+  if (depth > 90) return '#ff0000'; // Red
+  else if (depth > 70) return '#ff4500'; // OrangeRed
+  else if (depth > 50) return '#ff8c00'; // DarkOrange
+  else if (depth > 30) return '#ffd700'; // Yellow
+  else if (depth > 10) return '#9acd32'; // YellowGreen
+  else return '#00ff00'; // Green
+}
 
-  L.geoJson(data, {
-    pointToLayer: function (feature, latlng) {
-      return L.circleMarker(latlng, style(feature));
-    },
-    
-        // Binding a popup to each layer
-        onEachFeature: function(feature, layer) {
-          layer.bindPopup("<strong>" + feature.properties.place + "</strong><br /><br />Magnitude: " +
-            feature.properties.mag + "<br /><br />depth: " + feature.geometry.coordinates[2]);
-        }
-      }).addTo(earthquakeGroup);
-
-      earthquakeGroup.addTo(myMap);
-
-      
-      let legend = L.control({ position: "bottomright" });
-
-      legend.onAdd = function() {
-
-          let div = L.DomUtil.create("div", "info legend");
-          //labels = ["<10", "10-30", "30-50", "50-70", "70-90", "90+"];
-          let grades = [-10, 10, 30, 50, 70, 90];
-          let colors = ["#98ee00","#d4ee00","#eecc00","#ee9c00","#ea822c","#ea2c2c"];
-      // loop through our density intervals and generate a label with a colored square for each interval
-      for (let i = 0; i < grades.length; i++) {
-          div.innerHTML +=
-              '<i style="background:' + colors[i] + '">&emsp;&emsp;</i> '
-              + grades[i]
-              + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-      }
-
-      return div;
+// Fetch the data
+d3.json(queryUrl).then(function(data) {
+  // Create a GeoJSON layer
+  L.geoJSON(data, {
+    pointToLayer: function(feature, latlng) {
+      var geojsonMarkerOptions = {
+        radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
       };
-
-      legend.addTo(myMap);
-
-
-});
-  
-// Get the data with d3.
-d3.json(TECTONIC_URL).then(function(plate_data) {  
- 
-  let tectonicPlates = L.geoJson(plate_data, {
-    style: {
-      color: "blue",
-      weight: 2,
-      opacity: 1
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    },
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2] + " km");
     }
-  });
-    
-  tectonicPlates.addTo(tectonicGroup);
-  tectonicGroup.addTo(myMap);
+  }).addTo(myMap);
+
+  // Add legend with background and color palette
+  var legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [-10, 10, 30, 50, 70, 90],
+        labels = [];
+
+    // Add a white background to the legend
+    div.style.backgroundColor = '#fff';
+    div.style.padding = '10px';
+    div.style.margin = '5px';
+    div.style.borderRadius = '5px';
+    div.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.2)';
+
+    // Loop through depth intervals and generate label with colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + markerColor(grades[i] + 1) + '; width: 18px; height: 18px; display: inline-block; margin-right: 5px;"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] : '+');
+      div.innerHTML += '<br>';
+    }
+
+    return div;
+  };
+
+  legend.addTo(myMap);
 });
-
-
-var layersControl = L.control.layers(null, null, { collapsed: false });
-
-// Add overlay layers to layer control
-layersControl.addOverlay(earthquakeGroup, "Earthquakes");
-layersControl.addOverlay(tectonicGroup, "Tectonic Plates");
-layersControl.addBaseLayer(street, "Map");
-layersControl.addBaseLayer(topo, "Topo Map");
-
-// Add the layer control to the map
-layersControl.addTo(myMap);
